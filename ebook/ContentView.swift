474c48785ab49e31de6d5a2c3c499ea78bfefb22
andy <andy870622@gmail.com>
2020-10-18 19:06:38 +0800

    Initial Commit

---
+++ ebook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ebook
+//
+//  Created by User15 on 2020/10/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ebook/ebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  ebookApp.swift
+//  ebook
+//
+//  Created by User15 on 2020/10/18.
+//
+
+import SwiftUI
+
+@main
+struct ebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
